904295e103681d56d802fb8833d15138d4ea6a8a
Dymych152 <st-sn@mail.ru>
2024-07-23 15:14:00 +0300

    Первый коммит

---
+++ index.js
@@ -0,0 +1,33 @@
+/*for (let i = 0; i<5; i+=1) {
+    console.log(`Эта строка выведется 5 раз ${i}`);    
+}
+    
+const text = 'Эта строка выведется 5 раз';
+const count = 5;
+let i=0;
+while (i<count) {
+    console.log(`text ${i}`);
+    i +=1;
+}
+  
+
+
+const numMax = 2022;
+let count = 0;
+for (let numMin=1800; numMin<=numMax; numMin+=4) {
+    //console.log(`${numMin} Высокосный год ${count+=1} итераций`); 
+    count+=1  
+}
+console.log(`Всего было ${count} итераций и соответственно высокосных годов`);
+
+/*let count = 0;
+const dateG = 1961;
+let numMin = 1800;
+for (numMin ;numMin<dateG; numMin +=1 ){
+    count+=1;
+}
+    hello
+console.log(`Гагарин полетел в космос в ${numMin} году. Найдено за ${count} итераций`);
+*/
+
+
